3c2aa18a5566d69bf6e8999f9cb7b2905d0092e0
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-26 18:20:02 +0800

    Update PS3_3.R
    using anova analysis

--- PS3_3.R
+++ PS3_3.R
@@ -13,3 +13,28 @@ print(b)
 print(c)
 
 #观察结果，三组数据无统计的差异性，无法得出素食主义者比非素食主义者的锌含量更低的结论
+
+# MingYANG noticed:
+# using anova analysis may be more suitable:
+preveg<-"initiate"
+preveg[1:6]<-"pregnveg"
+preveg[7:23]<-"pregvege"
+preveg[19:23]<-"uprgveg"
+
+PNV<-c(185,189,187,181,150,176)
+PV<-c(171,174,202,171,207,125,189,179,163,174,184,186)
+UPV<-c(210,139,172,198,177)
+
+zinc <- c(PNV,PV,UPV)
+Pzinc<-cbind(PNV,PV,UPV)
+
+boxplot(Pzinc,xlab="preveg",ylab="zinc",col=c(2:4))
+
+prevegdata<-cbind(preveg,zinc)
+data_tbl <- as_tibble(prevegdata)
+datatblpregnveg <- data_tbl %>% 
+  filter(preveg != "uprgveg")
+
+anova_one_way <- aov(zinc ~ preveg, data = datatbl)
+summary(anova_one_way)
+# the end
